8b4f919e0daf9c1633036fa35c97cc275f448c09
ovi ahsan <shakil.dev@excelbd.com>
2025-02-20 16:06:25 +0600

    new commit

--- API/obj/Release/net8.0/API.AssemblyInfo.cs
+++ API/obj/Release/net8.0/API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7addf81ec9b7bf14505189bd94b9a5ecc05fe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
